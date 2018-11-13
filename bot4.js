2d75de18d8d219f538fd5662e34db3815699b14d
kokoseda <kokoseda22@yahoo.com>
2018-11-14 00:26:10 +0200

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -12,7 +12,7 @@ client.on('message', message => {
   if (!message.content.startsWith(PrEfix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== '468479699543130132') return;
+  if (message.author.id !== '299836492564529152') return;
 if (message.content.startsWith(PrEfix + 'wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**✅  : ${argresult}**`)
