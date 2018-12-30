50c7f3fa07ee99788cd64a3dae9ddfc1fdb61798
kokoseda <kokoseda22@yahoo.com>
2018-12-30 14:16:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
   console.log(`bot online by Fr3onGamer`);
   console.log('=======================================')
 });
-const developers = ["314135031029170197"]
+const developers = ["517409391570583574"]
 const adminprefix = "-";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
